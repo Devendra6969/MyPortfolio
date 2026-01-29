38ce598528a0eedc46aef706b63491bddcc1f40f
Devendra Singh <ds493133@gmail.com>
2026-01-29 21:26:11 +0530

    fix: reeact-router path fixed

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Navbar from './components/navbar/Navbar';
+import AboutCard from './components/card/AboutCard';
+import About from './pages/About';
+import Skills from './pages/Skills';
+import FooterOne from './components/footer/FooterOne';
+
+function App() {
+ 
+ 
+  return (
+    <>
+      
+      <FooterOne />
+    </>
+  );
+}
+
+export default App
